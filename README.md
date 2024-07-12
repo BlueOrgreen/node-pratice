0dd4cbb82bc085092ec4a139eac34b269fc82878
chenyunfan <chenyunfan@heytea.com>
2024-07-12 15:38:20 +0800

    feat: node学习

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Node 知识点
 
+```yaml
+key1:
+  child-key1: { name: 'zhangsan', age: 26 }
+  child-key2: "Test"
+key2:
+  - Chen
+  - true
+  - 30
+```
+
 #### 如何执行 ts 文件
 npm i ts-node
 
@@ -18,6 +28,29 @@ import { ensureFileSync } from 'fs-extra';
 
 // 确定目标路径是否存在, 没有则会创建文件
 ensureFileSync(path)
+// 如果没加上 utf-8 读取出来的是二进制的形式
 const content = readFileSync(path, "utf-8")
 ```
+
+
+#### 读取配置文件
+
+npm i find-up
+**find-up 可以往上寻找文件 并输出文件的路径**
+
+npm i dotenv  
+**dotenv 可以将读取的配置转换成对象的形式**
+
+```ts
+import { parse } from "dotenv";
+import findUp from "find-up";
+
+const search = findUp.sync(['.env']);
+const readFile = readFileSync(search);
+
+// 可以得到对象形式的数据
+const content = parse(readFile);
+
+```
+
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { resolve } from "path";
-import { isNil, set } from "lodash"
+import { has, isNil, omit, set } from "lodash"
 import { ensureFileSync } from 'fs-extra';
+import { parse } from 'dotenv';
 import { readFileSync, writeFileSync } from "fs";
 import YAML from "YAML";
-
+import findUp from "find-up";
 class Storage {
     protected _path = resolve(__dirname, '../content/my.yml');
 
@@ -24,11 +25,11 @@ class Storage {
         
         if(!isNil(filePath)) {
             this._path = filePath;
-            console.log('test', ensureFileSync(this._path));
+            // console.log('test', ensureFileSync(this._path));
             
             ensureFileSync(this._path);
             const config = YAML.parse(readFileSync(this._path, 'utf8'))
-            console.log('config', config);
+            // console.log('config', config);
             this._config = isNil(config) ? {} : config;
         }
     }
@@ -36,11 +37,39 @@ class Storage {
     set<T>(key: string, value: T) {
         ensureFileSync(this.path);
         set(this._config, key, value);
-        writeFileSync(this.path, JSON.stringify(this.path, null, 4))
+        writeFileSync(this.path, YAML.stringify(this._config, null, 4))
+    }
+
+    remove(key: string) {
+        this._config = omit(this._config, [key]);
+        if(has(this._config, key)) omit(this._config, [key]);
+        writeFileSync(this.path, YAML.stringify(this._config, null, 4));
+
     }
 }
 
 const storage = new Storage(resolve(__dirname, '../content/my.yml'));
 console.log(storage);
 
-storage.set('key3', [1, 2, 'hhh', true])
+// storage.set('key3', [11, 22, 'Test', false]);
+
+// storage.remove('key3');
+
+class Env {
+    /**
+     * 加载环境变量
+     */
+    async load() {
+        console.log(process.env.NODE_ENV);
+        if (isNil(process.env.NODE_ENV)) process.env.NODE_ENV = 'dev';
+        const search = [findUp.sync(['.env'])];
+        console.log('search===>', search);
+        const readContent = readFileSync(search[0], 'utf-8')
+        console.log('readContent===>', readContent);
+        const docContent = parse(readContent);
+        console.log('readFile Content parse docContent===>', docContent);
+    }
+}
+
+const env = new Env();
+env.load();
